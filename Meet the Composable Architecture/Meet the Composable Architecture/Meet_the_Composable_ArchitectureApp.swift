e92b389af5dee6ddcbf188955f70d1295a4fa8bc
Alvaro <alvher.orellana@gmail.com>
2025-08-14 12:43:44 -0400

    Initial files for project Meet the composable Architecture

---
+++ Meet the Composable Architecture/Meet the Composable Architecture/Meet_the_Composable_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meet_the_Composable_ArchitectureApp.swift
+//  Meet the Composable Architecture
+//
+//  Created by Alvaro Orellana on 02-08-25.
+//
+
+import SwiftUI
+
+@main
+struct Meet_the_Composable_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
